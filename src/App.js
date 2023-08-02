81b72297b2bb348def1758cac39da73a39182033
Jean <jvictor4919@gmail.com>
2023-08-01 21:57:41 -0300

    buscar Cep

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+
+//npm install react-icons
+import { FiSearch } from 'react-icons/fi';
+import styles from './style.css';
+import { useState } from 'react';
+import api from './services/api';
 
 function App() {
+
+  const [cep, setCep] = useState('');
+  const[cepResponse, setCepResponse] = useState({});
+
+  async function handleSearch(){
+    
+    if(cep === ''){
+      alert('preencha algum CEP');
+      return;
+    }
+    try{
+      const response = await api.get(`${cep}/json`);
+      setCepResponse(response.data);
+      setCep('');
+    }catch{
+      alert('Erro ao buscar Cep');
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+        <h1 className='title'>Buscador de CEP</h1>
+
+        <div className='containerInput'>
+          <input 
+            type="text"
+            placeholder='Digite o CEP'
+            value={cep}
+            onChange={(e) => setCep(e.target.value)}
+          />
+          <button className='buttonSearch' onClick={handleSearch}>
+            <FiSearch size={25} color="#fff" />
+          </button>
+        </div>
+        {/* verificando se há resposta de busca */}
+        {Object.keys(cepResponse).length > 0 && (
+          <main className='main'> 
+          <h2>CEP: {cepResponse.cep}</h2>
+          <span>Logradouro: {cepResponse.logradouro}</span>
+          {cepResponse.complemento.length > 0 && ( <span> Complemento: {cepResponse.complemento}</span>)}
+          <span>Bairro: {cepResponse.bairro}</span>
+          <span>Cidade: {cepResponse.localidade} - {cepResponse.uf}</span>
+
+        </main>
+        )}
+        
+
     </div>
   );
 }
